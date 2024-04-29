ff864b989484421492fa39aeae0dd3a5c7677815
Hema Devi <uhemadevi1927@gmail.com>
2024-04-29 21:05:56 +0530

    pair amm functions

--- src/Pair.sol
+++ src/Pair.sol
@@ -48,22 +48,62 @@ contract Pair {
         }
     _transferFrom(msg.sender, address(this), fractionalTokenAmount);
     lpToken.mint(msg.sender, lpTokenAmount);
-    emit Add(baseTokenAmount, fractionalTokenAmount, lpTokenAmount);
+emit Add(baseTokenAmount, fractionalTokenAmount, lpTokenAmount);
 
 
     }
-    function remove()
+    function remove(uint256 lpTokenAmount,uint256 minBaseTokenOutputAmount,uint256 minFractionalTokenOutputAmount)public returns (uint256 baseTokenOutputAmount,uint256 fractionalTokenOutputAmount)
     {
+        (baseTokenOutputAmount, fractionalTokenOutputAmount) = removeQuote(lpTokenAmount);
+
+        require(baseTokenOutputAmount >= minBaseTokenOutputAmount,"Slippage: base token amount out");
+        require(fractionalTokenOutputAmount >= minFractionalTokenOutputAmount,"Slippage: fractional token amount out");
+        _transferFrom(address(this), msg.sender, fractionalTokenOutputAmount);
+
+        lpToken.burn(msg.sender, lpTokenAmount);
+
+        if (baseToken == address(0)) {
+            msg.sender.safeTransferETH(baseTokenOutputAmount);
+        } else {
+            ERC20(baseToken).safeTransfer(msg.sender, baseTokenOutputAmount);
+        }
+
+        emit Remove(baseTokenOutputAmount,fractionalTokenOutputAmount,lpTokenAmount);
 
     }
-    function buy()
-    {
+    function buy( uint256 outputAmount,uint256 maxInputAmount) public payable returns (uint256) {
+        uint256 inputAmount = buyQuote(outputAmount);
+        require(inputAmount <= maxInputAmount, "Slippage: amount in");
+        require(baseToken == address(0)? msg.value == maxInputAmount: msg.value == 0,"Invalid ether input");
+        _transferFrom(address(this), msg.sender, outputAmount);
+
+        if (baseToken == address(0)) {
+            uint256 refundAmount = maxInputAmount - inputAmount;
+            if (refundAmount > 0) msg.sender.safeTransferETH(refundAmount);
+        } else {
+            ERC20(baseToken).safeTransferFrom(msg.sender,address(this),inputAmount);
+        }
+
+        emit Buy(inputAmount, outputAmount);
+
+        return inputAmount;
 
     }
-    function sell()
-    {
+    function sell(uint256 inputAmount, uint256 minOutputAmount) public returns (uint256) {
+        uint256 outputAmount = sellQuote(inputAmount);
+        require(outputAmount >= minOutputAmount, "Slippage: amount out");
+        _transferFrom(msg.sender, address(this), inputAmount);
+        if (baseToken == address(0)) {
+            msg.sender.safeTransferETH(outputAmount);
+        } else {
+            ERC20(baseToken).safeTransfer(msg.sender, outputAmount);
+        }
 
+        emit Sell(inputAmount, outputAmount);
+
+        return outputAmount;
     }
+
  //nft amm
     function nftAdd()
     {
